b9608327634fc750f9d741f6efcfe9dc37b29154
Deepesh Aggarwal <deepeshnith547@gmail.com>
2016-09-17 13:05:16 -0700

    renamed file to sqldump.sh

--- sqldump.sh
+++ sqldump.sh
@@ -4,6 +4,7 @@ SERVER="127.0.0.1"
 USERNAME="deepesh"
 DATABASE="database"
 echo "Dumping of data of $DATABASE from $SERVER"
+
 ssh $USERNAME@$SERVER mysqldump --complete-insert --lock-all-tables --no-create-db --no-create-info --extended-insert=FALSE --password=root -u root $DATABSE > $DATABASE.sql
 
 scp $USERNAME@$SERVER:~/$DATABASE.sql $HOME/$DATABASE.sql
